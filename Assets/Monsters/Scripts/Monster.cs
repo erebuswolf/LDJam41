bf415da563ab5c25b1d8468d97df9ed319f0fa2a
iradicator <iradicator@gmail.com>
2018-04-21 02:33:50 -0700

    introducing monsters

---
+++ Assets/Monsters/Scripts/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform target;
+    [SerializeField] private ParticleSystem particlesPrefab;
+
+    private void Update()
+    {
+        Vector3 toTargetDir = (target.position - transform.position).normalized;
+        transform.position += speed * Time.deltaTime * toTargetDir;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == target.gameObject)
+        {
+            var effect = GameObject.Instantiate(particlesPrefab, transform.position, particlesPrefab.transform.rotation);
+            effect.GetComponent<ParticleSystem>().Play();
+            Destroy(effect, 5.0f);
+            Destroy(gameObject);            
+        }
+    }
+}
