467418ea4f981b68d350054b983fb8ec7944ea06
Jesse Fish <jesfish@gmail.com>
2018-04-21 15:44:02 -0700

    Add player data file
    Add player data and spawn points.

---
+++ Assets/Player/PlayerData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class to manage player information that should never be despawned or destroyed.
+public class PlayerData : MonoBehaviour {
+    [SerializeField]
+    private SpawnPoint LastSpawnPoint;
+
+    [SerializeField]
+    private float resources;
+
+    // Use this for initialization
+    void Start() { 
+
+    }
+
+    public void AddResources(float resourcesAdded) {
+        this.resources += resourcesAdded;
+    }
+    
+    public float GetResources() {
+        return resources;
+    }
+
+    public void SpendResources(float resourcesSpent) {
+        if (this.resources >= resourcesSpent) {
+            this.resources -= resourcesSpent;
+            return;
+        }
+
+        Debug.LogWarning("Spending Resources We don't have!!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
