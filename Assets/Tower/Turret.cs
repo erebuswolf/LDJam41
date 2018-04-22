083178622061e1c6ab37c7d9aa4c354ec41553ea
Jesse Fish <jesfish@gmail.com>
2018-04-22 00:04:39 -0700

    Can activate turrets correctly
    Can activate and actively change the active turret with the car, interrupting the animation works fine.

--- Assets/Tower/Turret.cs
+++ Assets/Tower/Turret.cs
@@ -4,99 +4,48 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour {
 
-    private bool switching;
-
     [SerializeField] private Monster trackedMonster;
-    private const float lookSpeed = 300f;
-
-    private Quaternion startRotation;
-
-    private bool Activated;
+    private const float lookSpeed = 600f;
+    [SerializeField] private bool lowered;
 
     [SerializeField] AudioSource LaserShot;
 
 	// Use this for initialization
 	void Start () {
-        startRotation = transform.rotation;
-        switching = false;
-        StartCoroutine(TrackMonster());
-	}
+        StartCoroutine(UpdateLookAtRoutine());
+        lowered = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    public void SwitchShootingModeAnimation() {
-        if (switching) {
-            return;
-        }
-        switching = true;
-        StartCoroutine(SwitchAnimation());
+    public void Lower() {
+        lowered = true;
     }
 
-    public bool isSwitching() {
-        return switching;
+    public void Raised() {  
+        lowered = false;
     }
 
-    public void ActivateTurret() {
-        Activated = true;
-    }
-
-    IEnumerator SwitchAnimation () {
-        Vector3 raisedPos = this.gameObject.transform.position;
-        Vector3 loweredPos = this.gameObject.transform.position + Vector3.down * 30; // Move it down
-        float speed = 2;
-        float delta = 0;
-        Debug.LogFormat("Delta: {0}", delta);
-
-        Vector3 move;
-        // Lower the turret
-        while (delta < 1)
-        {
-            delta += speed * Time.deltaTime;
-            delta = Mathf.Clamp01(delta);
-
-            move = Vector3.Lerp(raisedPos, loweredPos, delta);
-            Debug.LogFormat("Delta: {0}, {1}, {2}, {3}", move , delta, raisedPos, loweredPos);
-            this.gameObject.transform.position = move;
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(.5f);
-
-        // Raise the turret back up
-        while (delta > 0)
-        {
-            delta -= speed * Time.deltaTime;
-            delta = Mathf.Clamp01(delta);
-            move = Vector3.Lerp(raisedPos, loweredPos, delta);
-            this.gameObject.transform.position = move;
-            yield return null;
-        }
-
-        switching = false;
-    }
-
-    IEnumerator TrackMonster() {
+    IEnumerator UpdateLookAtRoutine() {
         while(true) {
             float speed = Time.deltaTime * lookSpeed;
-            Quaternion goalRotation = startRotation;
+            Quaternion goalRotation = (gameObject.transform.position.x > 0) ? Quaternion.Euler(0, -90, 0) : Quaternion.Euler(0, 90, 0);
             Quaternion currentRotation = gameObject.transform.rotation;
-            if (trackedMonster == null || !trackedMonster.isAlive() || !Activated) {
+            if (trackedMonster == null || !trackedMonster.isAlive() || lowered) {
+                if (lowered) {
+                    goalRotation = Quaternion.Euler(-90,0,0);
+                }
             } else {
                 goalRotation = Quaternion.LookRotation(
                     trackedMonster.GetHeadPosition()
                     - gameObject.transform.position);
-
             }
             gameObject.transform.rotation = Quaternion.RotateTowards(currentRotation, goalRotation, speed);
             yield return null;
         }
-    }
-
-    public void TurnTurretAnimation () {
-        // Animate to turn at a 90 degree angle
     }
 
     public void TrackTarget (Monster target) {
---
+++ Assets/Tower/TurretController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum ControllerState { IDLE = 0, RAISING, LOWERING};
+public class TurretController : MonoBehaviour {
+
+    private bool switching;
+    
+    [SerializeField] private List<Turret> turrets;
+    
+    [SerializeField] private float stowTurretHeight;
+
+    [SerializeField] private int activeTurretIndex;
+    [SerializeField] private int nextTurretIndex;
+
+    [SerializeField] private ControllerState controllerState;
+
+    float speed = 40;
+
+    // Use this for initialization
+    void Start () {
+        switching = false;
+        activeTurretIndex = -1;
+        controllerState = ControllerState.IDLE;
+    }
+	
+    private void loweringTurret() {
+        if(activeTurretIndex == -1) {
+            return;
+        }
+        turrets[activeTurretIndex].Lower();
+        Vector3 startPos = turrets[activeTurretIndex].gameObject.transform.localPosition;
+        Vector3 endPos = startPos;
+        endPos.y = stowTurretHeight;
+        
+        float moveDst = speed * Time.deltaTime;
+
+        Vector3 move = Vector3.MoveTowards(startPos, endPos, moveDst);
+        turrets[activeTurretIndex].gameObject.transform.localPosition = move;
+        
+        if (Mathf.Approximately(0, (move - endPos).sqrMagnitude)) {
+            // We have reached our goal state
+            controllerState = ControllerState.RAISING;
+            activeTurretIndex = nextTurretIndex;
+            nextTurretIndex = -1;
+        } else {
+
+        }
+    }
+
+    private void raisingTurret() {
+        Vector3 startPos = turrets[activeTurretIndex].gameObject.transform.localPosition;
+        Vector3 endPos = startPos;
+        endPos.y = 0;
+
+        Vector3 move;
+        float moveDst = speed * Time.deltaTime;
+
+        move = Vector3.MoveTowards(startPos, endPos, moveDst);
+        turrets[activeTurretIndex].gameObject.transform.localPosition = move;
+        
+        if (Mathf.Approximately(0, (move - endPos).sqrMagnitude)) {
+            // We have reached our goal state
+            controllerState = ControllerState.IDLE;
+            turrets[activeTurretIndex].Raised();
+            switching = false;
+        } else {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (controllerState) {
+            case ControllerState.LOWERING:
+                loweringTurret();
+                break;
+            case ControllerState.RAISING:
+                raisingTurret();
+                break;
+            case ControllerState.IDLE:
+                switching = false;
+                break;
+        }
+    }
+
+    public Turret getActiveTurret() {
+        if (switching || activeTurretIndex == -1) {
+            return null;
+        }
+        return turrets[activeTurretIndex];
+    }
+
+    public void SwitchShootingModeAnimation(ShootingMode mode) {
+        if ((int)mode == activeTurretIndex) {
+            return;
+        }
+
+        switching = true;
+        if (activeTurretIndex == -1) {
+            activeTurretIndex = (int)mode;
+            controllerState = ControllerState.RAISING;
+        } else {
+            nextTurretIndex = (int)mode;
+            switch (controllerState) {
+                case ControllerState.RAISING:
+                case ControllerState.IDLE:
+                    controllerState = ControllerState.LOWERING;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public bool isSwitching() {
+        return switching;
+    }
+    
+}
