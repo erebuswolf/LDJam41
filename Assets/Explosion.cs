8cf8a5d5cd3316d9ea33c909ce9344adfdbcbf74
Jesse Fish <jesfish@gmail.com>
2018-04-23 12:23:40 -0700

    Added lighting to the introduction of the game

---
+++ Assets/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    bool exploded;
+    public void Explode() {
+        exploded = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (exploded) {
+            float y = Mathf.MoveTowards(this.transform.position.y, 0, 50);
+            this.transform.position = new Vector3(this.transform.position.x, y, this.transform.position.z);
+        }
+    }
+}
---
+++ Assets/LightPulse.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPulse : MonoBehaviour {
+    [SerializeField] Light light;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator PulseRoutine() {
+        while (light.intensity < 5) {
+            light.intensity = Mathf.MoveTowards(light.intensity, 5, .5f);
+            yield return null;
+        }
+        yield return new WaitForSeconds(.06f);
+        while (light.intensity > 0) {
+            light.intensity = Mathf.MoveTowards(light.intensity, 0, .2f);
+            yield return null;
+        }
+    }
+
+    public void Pulse () {
+        StartCoroutine(PulseRoutine());
+    }
+
+    IEnumerator StrobeRoutine() {
+        float startTime = Time.time;
+        while (Time.time - startTime < 3) {
+            light.intensity = 5;
+            yield return new WaitForSeconds(.06f);
+            light.intensity = 0;
+            yield return new WaitForSeconds(.06f);
+        }
+        light.intensity = 5;
+    }
+    public void Strobe() {
+
+        StartCoroutine(StrobeRoutine());
+    }
+
+    public void StayOnAndFlicker() {
+
+    }
+
+}
