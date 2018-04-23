4000c01de7c9a4a19155c18656df56e177c43d7d
Jesse Fish <jesfish@gmail.com>
2018-04-23 10:27:53 -0700

    Reactor now has explosion sounds when being hit
    there are now explosion sounds and reaction sounds when the reactor is hit.

--- Assets/Reactor/Reactor.cs
+++ Assets/Reactor/Reactor.cs
@@ -8,8 +8,14 @@ public class Reactor : MonoBehaviour {
 
     private float startingHealth;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private AudioSource reactorExplosion;
+
+
+    [SerializeField] private List< AudioSource> hitReactionSound;
+
+    // Use this for initialization
+    void Start () {
         startingHealth = health;
 	}
 	
@@ -26,10 +32,21 @@ public class Reactor : MonoBehaviour {
         return health;
     }
 
+    IEnumerator ReactionSound() {
+        if (health == 7) {
+            yield return new WaitForSeconds(1);
+            hitReactionSound[0].Play();
+        } else if (health == 4) {
+            hitReactionSound[1].Play();
+        }
+    }
+    
     public void RecieveHit(float healthLoss) {
         health -= healthLoss;
         // A game manager or something else should query the health state of this object.
         // And manage game loss.
+        reactorExplosion.Play();
+        StartCoroutine(ReactionSound());
         if (health <= 0) {
             FindObjectOfType<EndGameIU>().Lose();
         }
