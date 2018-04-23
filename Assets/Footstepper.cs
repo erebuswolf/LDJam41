c188a82ade300bb9283e4ab0429c3c6f9945edb2
Jesse Fish <jesfish@gmail.com>
2018-04-23 05:19:10 -0700

    Monsters now have footsteps
    Monsters now have footsteps that are real!!!

---
+++ Assets/Footstepper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstepper : MonoBehaviour {
+    [SerializeField] private AudioSource Footstep;
+    private Shaker PlayerCam;
+    // Use this for initialization
+    void Start () {
+        PlayerCam = FindObjectOfType<Shaker>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CheckPlayerCameraShake() {
+        float dist = (PlayerCam.transform.position - this.transform.position).magnitude;
+        if (dist < 500) {
+            PlayerCam.Shake(2, .1f * Mathf.Clamp(500/ dist, 0, 20)/3f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == 10) {
+            Footstep.Play();
+            CheckPlayerCameraShake();
+        }
+    }
+}
