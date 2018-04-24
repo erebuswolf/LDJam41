e16b45186dcf19d60b55411fab7471b8d6c69552
Jesse Fish <jesfish@gmail.com>
2018-04-23 17:14:43 -0700

    Bridge now breaks!

---
+++ Assets/BridgeBreak.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBreak : MonoBehaviour {
+
+    [SerializeField] List<GameObject> BridgePieces = new List<GameObject>();
+
+    [SerializeField] GameObject hiddenBridge;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.layer == 9) {
+            BreakBridge();
+        }
+    }
+
+    public void BreakBridge() {
+        hiddenBridge.active = true;
+        foreach(GameObject g in BridgePieces) {
+            g.active = false;
+        }
+    }
+}
